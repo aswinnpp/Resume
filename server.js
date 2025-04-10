79874e1b0ee2a865746ead08c8279a194629d86b
Aswin Cp <aswincp554@gmail.com>
2025-04-10 23:57:57 +0530

    db connection

--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-const express = require('express');
-const path = require('path');
-const userRoute = require("./routes/user")
-const adminRoute= require("./routes/admin")
+const express = require("express");
+const path = require("path");
+const userRoute = require("./routes/user");
+const env = require("dotenv").config();
+const session = require("express-session");
+const connectDB = require("./dataBase/connectDB");
+const adminRoute = require("./routes/admin");
 const app = express();
 
 // Set the view engine to EJS
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+connectDB()
 
 // app.use("/admin", adminRoute);
 app.use("/", userRoute);
 
 console.log("User routes loaded");
 
-
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
