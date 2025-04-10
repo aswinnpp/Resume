630d5c74cf11f751b000358c1b1c6ff18e1ad8c6
Aswin Cp <aswincp554@gmail.com>
2025-04-10 23:15:11 +0530

    new file

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const homeController = require("../controllers/userController/homeController");
+
+router.get("/", homeController.loadHome)
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,30 @@
-const express  = require("express")
-const app = express()
-const PORT = 3000;
+const express = require('express');
+const path = require('path');
+const userRoute = require("./routes/user")
+const adminRoute= require("./routes/admin")
+const app = express();
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.get('/', (req, res) => {
-    res.send('Hello, worldn vvhjhenfvjjfnv!');
-  });
-  
+
+// app.use("/admin", adminRoute);
+app.use("/", userRoute);
+
+console.log("User routes loaded");
 
 
 
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
-  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
